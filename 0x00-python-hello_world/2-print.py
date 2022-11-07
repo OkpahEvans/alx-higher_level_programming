d0249f1c379a83512f6b63071e070d367f9af61c
OkpahEvans <okpah@gmail.com>
2022-11-07 15:54:22 -0800

    Python Basis

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-Hello_World = "Programming is like building a multilingual puzzle"
-print("\"Hello_World)
+Hello_World = "\"Programming is like building a multilingual puzzle"
+print(Hello_World)
