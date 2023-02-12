0ccce1744a9d055c41cf4115291f66ad66eb6796
Okpah Evans Chukwunnaemeka <105133449+OkpahEvans@users.noreply.github.com>
2023-02-11 18:19:35 -0800

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,37 +1,44 @@
-#!/usr/bin/python3
-"""4. Access and update private attribute"""
+  #!/usr/bin/python3
+""" class Square that defines a square"""
 
 
 class Square:
-        """Represents a class square defined by.
-            Private instance attribute: size:
-                    - property def size(self)
-                            - property setter def size(self, value)
-                                Instantiation with optional size.
-                                    Public instance method: def area(self).
-                                        """
+    """ class Square that defines a square"""
+    def __init__(self, size=0):
+        """ init square
 
-                                            def __init__(self, size=0):
-                                                        """Args:
-                                                                    self: object instance
-                                                                                size: size of the square passed. Defaults to 0
-                                                                                        """
-                                                                                                self.__size = size
+        Args:
+            value (int): size of the square.
+        """
+        self.size = size
 
-                                                                                                    @property
-                                                                                                        def size(self):
-                                                                                                                    """Retrieves the size."""
-                                                                                                                            return self.__size
+    @property
+    def size(self):
+        """int: private size.
 
-                                                                                                                            @size.setter
-                                                                                                                                def size(self, value):
-                                                                                                                                            """Sets the size to a value."""
-                                                                                                                                                    if not isinstance(value, int):
-                                                                                                                                                                    raise TypeError("size must be an integer")
-                                                                                                                                                                        elif value < 0:
-                                                                                                                                                                                        raise ValueError("size must be >= 0")
-                                                                                                                                                                                            self.__size = value
+        Returns:
+            Private size.
+        """
+        return self.__size
 
-                                                                                                                                                                                                def area(self):
-                                                                                                                                                                                                            """Returns the current square area."""
-                                                                                                                                                                                                                    return self.__size ** 2
+    @size.setter
+    def size(self, value):
+        """Sets value into size, must be int.
+
+        Args:
+            value (int): size of the square.
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value  #: size of the square
+
+    def area(self):
+        """returns the area
+
+        Returns:
+            area.
+        """
+        return self.__size**2
