f88d31641b3291e6f2b0b32b350e4e3f7d3ba0de
Okpah Evans Chukwunnaemeka <105133449+OkpahEvans@users.noreply.github.com>
2023-03-03 03:18:07 -0800

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Script that fetches https://intranet.hbtn.io/status"""
+"""fetches https://intranet.hbtn.io/status."""
+import requests
 
 
 if __name__ == "__main__":
-    import requests
-    res = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(res.text)))
-    print('\t- content: {}'.format(res.text))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
