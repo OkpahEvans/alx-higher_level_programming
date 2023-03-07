56acdd78b4f009a9ab2dfaa3cbac19942752d1b0
OkpahEvans <okpahevans@gmail.com>
2023-03-07 03:00:50 -0800

    Closures

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+class Rectangle {
+	  constructor (w, h) {
+		      if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+			            this.width = w;
+			            this.height = h;
+			          }
+		    }
+
+	  print () {
+		      console.log(('X'.repeat(this.width) + '\n').repeat(this.height).split('')
+			            .slice(0, -1).join(''));
+		    }
+}
+module.exports = Rectangle;
