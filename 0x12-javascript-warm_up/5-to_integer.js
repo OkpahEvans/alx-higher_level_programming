3166943559dcc9614c60f8b52f02d1911f709a47
Okpah Evans Chukwunnaemeka <105133449+OkpahEvans@users.noreply.github.com>
2023-03-06 20:49:33 -0800

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const n = parseInt(process.argv[2]);
-if (!isNaN(n)) {
-	  console.log('My number: ' + n);
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
 } else {
-	  console.log('Not a number');
+  console.log('My number:', parseInt(process.argv[2]));
 }
