3ae82171f54c6b0bb5009436b562f201e024c0f4
Okpah Evans Chukwunnaemeka <105133449+OkpahEvans@users.noreply.github.com>
2023-03-07 03:14:06 -0800

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,21 @@
 #!/usr/bin/node
 class Rectangle {
-	  constructor (w, h) {
-		      if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
-			            this.width = w;
-			            this.height = h;
-			          }
-		    }
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	  print () {
-		      console.log(('X'.repeat(this.width) + '\n').repeat(this.height).split('')
-			            .slice(0, -1).join(''));
-		    }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
 }
+
 module.exports = Rectangle;
